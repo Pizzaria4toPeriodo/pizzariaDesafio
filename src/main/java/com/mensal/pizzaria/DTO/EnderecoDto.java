a2060bad7d50a320b5d36933a65a3485c6661bd2
marcelonv011 <marcelonv011@gmail.com>
2023-08-16 20:55:21 -0300

    endereco dto

--- src/main/java/com/mensal/pizzaria/DTO/EnderecoDto.java
+++ src/main/java/com/mensal/pizzaria/DTO/EnderecoDto.java
@@ -1,4 +1,48 @@
 package com.mensal.pizzaria.DTO;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
 public class EnderecoDto {
+
+
+    private Long id;
+
+    private String rua;
+
+    private int numero;
+
+    public EnderecoDto(Long id, String rua, int numero) {
+        this.id = id;
+        this.rua = rua;
+        this.numero = numero;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
 }
