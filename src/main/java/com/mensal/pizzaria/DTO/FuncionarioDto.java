c78db6e0d2c6bfd07b5216a3b5b40a27fbd47d72
marcelonv011 <marcelonv011@gmail.com>
2023-08-16 20:57:03 -0300

    funcionario dto pronto

---
+++ src/main/java/com/mensal/pizzaria/DTO/FuncionarioDto.java
@@ -0,0 +1,48 @@
+package com.mensal.pizzaria.DTO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+public class FuncionarioDto {
+
+    private Long id;
+
+
+    private String nome;
+
+    private String cargo;
+
+    public FuncionarioDto(Long id, String nome, String cargo) {
+        this.id = id;
+        this.nome = nome;
+        this.cargo = cargo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+}
