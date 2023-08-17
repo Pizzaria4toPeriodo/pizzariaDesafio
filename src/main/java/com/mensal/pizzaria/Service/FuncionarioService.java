954a521a17d086bfe2c6ff6edb8c4756b1e51257
marcelonv011 <marcelonv011@gmail.com>
2023-08-16 21:20:13 -0300

    funcionario service fazendo

--- src/main/java/com/mensal/pizzaria/Service/FuncionarioService.java
+++ src/main/java/com/mensal/pizzaria/Service/FuncionarioService.java
@@ -1,4 +1,29 @@
 package com.mensal.pizzaria.Service;
 
+import com.mensal.pizzaria.DTO.FuncionarioDto;
+import com.mensal.pizzaria.Entity.Funcionario;
+import com.mensal.pizzaria.Repository.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class FuncionarioService {
+
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+
+    @Transactional
+    public void cadastraFuncionario(FuncionarioDto funcionarioDto){
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome(funcionarioDto.getNome());
+        funcionario.setCargo(funcionarioDto.getCargo());
+        funcionarioRepository.save(funcionario);
+    }
+
+    @Transactional
+    public void atualizaFuncionario(FuncionarioDto funcionarioDto){
+
+    }
+
 }
