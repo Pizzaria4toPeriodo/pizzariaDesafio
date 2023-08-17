b60f898d625dbac86a9395a7f2d5bbeb83fac0fb
marcelonv011 <marcelonv011@gmail.com>
2023-08-17 12:29:00 -0300

    endereco get lista

--- src/main/java/com/mensal/pizzaria/Controller/EnderecoController.java
+++ src/main/java/com/mensal/pizzaria/Controller/EnderecoController.java
@@ -1,4 +1,28 @@
 package com.mensal.pizzaria.Controller;
 
+import com.mensal.pizzaria.Repository.EnderecoRepository;
+import com.mensal.pizzaria.Service.EnderecoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/enderecos")
 public class EnderecoController {
+
+    @Autowired
+    private EnderecoService enderecoService;
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
+    @GetMapping()
+    public ResponseEntity<?> listaCompleta(){
+        return ResponseEntity.ok(this.enderecoRepository.findAll());
+    }
+
+
+
 }
