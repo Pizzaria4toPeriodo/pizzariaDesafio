7f236a5fe8ffabb63be7af5b4e2013f8bae77e61
marcelonv011 <marcelonv011@gmail.com>
2023-08-17 11:02:58 -0300

    get path funcionario

--- src/main/java/com/mensal/pizzaria/Controller/FuncionarioController.java
+++ src/main/java/com/mensal/pizzaria/Controller/FuncionarioController.java
@@ -1,10 +1,12 @@
 package com.mensal.pizzaria.Controller;
 
+import com.mensal.pizzaria.Entity.Funcionario;
 import com.mensal.pizzaria.Repository.FuncionarioRepository;
 import com.mensal.pizzaria.Service.FuncionarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +24,16 @@ public class FuncionarioController {
     public ResponseEntity<?> listaCompleta(){
         return ResponseEntity.ok(this.funcionarioRepository.findAll());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(
+            @PathVariable("id") final long id
+    ) {
+        final Funcionario funcionario = this.funcionarioRepository.findById(id).orElse(null);
+        return funcionario == null
+                ? ResponseEntity.badRequest().body("Sem valor encontrado.")
+                : ResponseEntity.ok(funcionario);
+    }
+
 
 }
