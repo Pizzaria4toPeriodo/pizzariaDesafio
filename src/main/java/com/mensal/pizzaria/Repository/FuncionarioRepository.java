3be4e77166a38516e18179448d3318a7bd918484
marcelonv011 <marcelonv011@gmail.com>
2023-08-16 21:06:35 -0300

    endereco repository e funcionario pronto

---
+++ src/main/java/com/mensal/pizzaria/Repository/FuncionarioRepository.java
@@ -0,0 +1,10 @@
+package com.mensal.pizzaria.Repository;
+
+import com.mensal.pizzaria.Entity.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+
+}
