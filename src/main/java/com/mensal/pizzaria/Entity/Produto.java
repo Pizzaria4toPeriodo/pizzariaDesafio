4666c359eede8cd858d2f50fbe2880d846b7c4b3
gustavoemf <gustavoeduardomf@gmail.com>
2023-08-16 20:16:17 -0400

    Produto entity

--- src/main/java/com/mensal/pizzaria/Entity/Produto.java
+++ src/main/java/com/mensal/pizzaria/Entity/Produto.java
@@ -4,13 +4,23 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Entity
 @Getter @Setter
 @Table(name = "produto_table", schema = "pizzaria")
 public class Produto {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Getter @Setter
     @Column(name = "id", nullable = false,  unique = true)
     private Long id;
+
+    @Column(name = "nome", nullable = false, length = 30)
+    private String nome;
+
+    @Column(name = "tipo", nullable = false)
+    private String tipo;
+
+    @Column(name = "preco", nullable = false)
+    private BigDecimal preco;
 }
---
+++ src/main/java/com/mensal/pizzaria/Entity/Status_pagamento.java
@@ -0,0 +1,5 @@
+package com.mensal.pizzaria.Entity;
+
+public enum Status_pagamento {
+    PAGO, NAO_PAGO, AGUARDANDO_PAGAMENTO
+}
