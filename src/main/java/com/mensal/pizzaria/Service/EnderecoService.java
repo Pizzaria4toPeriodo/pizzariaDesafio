e555113c68055afa485c30328eb5f4beb23efede
marcelonv011 <marcelonv011@gmail.com>
2023-08-17 11:45:20 -0300

    endereco service salvar

--- src/main/java/com/mensal/pizzaria/Service/EnderecoService.java
+++ src/main/java/com/mensal/pizzaria/Service/EnderecoService.java
@@ -1,8 +1,11 @@
 package com.mensal.pizzaria.Service;
 
+import com.mensal.pizzaria.DTO.EnderecoDto;
+import com.mensal.pizzaria.Entity.Endereco;
 import com.mensal.pizzaria.Repository.EnderecoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class EnderecoService {
@@ -10,6 +13,11 @@ public class EnderecoService {
     @Autowired
     private EnderecoRepository enderecoRepository;
 
-
-
+    @Transactional
+    public void cadastrarEndereco(EnderecoDto enderecoDto){
+        Endereco endereco = new Endereco();
+        endereco.setRua(enderecoDto.getRua());
+        endereco.setNumero(enderecoDto.getNumero());
+        enderecoRepository.save(endereco);
+    }
 }
