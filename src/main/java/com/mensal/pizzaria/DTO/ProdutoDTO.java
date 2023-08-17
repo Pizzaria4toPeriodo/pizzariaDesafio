913aee02d7d3f827242f51c91eb2d1ca79db11bc
gustavoemf <gustavoeduardomf@gmail.com>
2023-08-16 20:23:43 -0400

    Produto DTO 2

--- src/main/java/com/mensal/pizzaria/DTO/ProdutoDTO.java
+++ src/main/java/com/mensal/pizzaria/DTO/ProdutoDTO.java
@@ -1,10 +1,5 @@
 package com.mensal.pizzaria.DTO;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
 import java.math.BigDecimal;
 
 public class ProdutoDTO {
@@ -12,4 +7,44 @@ public class ProdutoDTO {
     private String nome;
     private String tipo;
     private BigDecimal preco;
+
+    public ProdutoDTO() {}
+    public ProdutoDTO(Long id, String nome, String tipo, BigDecimal preco) {
+        this.id = id;
+        this.nome = nome;
+        this.tipo = tipo;
+        this.preco = preco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
 }
