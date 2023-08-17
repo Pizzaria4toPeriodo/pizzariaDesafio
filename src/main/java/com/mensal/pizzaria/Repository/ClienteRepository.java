adb4e81c55872c876000076c91920676d7f02b25
Luis <luiserr1394@gmail.com>
2023-08-17 20:10:43 -0300

    repositories prontos

---
+++ src/main/java/com/mensal/pizzaria/Repository/ClienteRepository.java
@@ -0,0 +1,10 @@
+package com.mensal.pizzaria.Repository;
+
+import com.mensal.pizzaria.Entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository <Cliente,Long> {
+
+
+    
+}
---
+++ src/main/java/com/mensal/pizzaria/Repository/EntregaRepository.java
@@ -0,0 +1,10 @@
+package com.mensal.pizzaria.Repository;
+
+
+import com.mensal.pizzaria.Entity.Entrega;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface EntregaRepository  extends JpaRepository <Entrega,Long>{
+
+
+
+}
