1de9c13108739552e9828ec7b6b39143a18b5782
marcelonv011 <marcelonv011@gmail.com>
2023-08-17 11:42:12 -0300

    endereco service comencando

--- src/main/java/com/mensal/pizzaria/Service/EnderecoService.java
+++ src/main/java/com/mensal/pizzaria/Service/EnderecoService.java
@@ -1,4 +1,15 @@
 package com.mensal.pizzaria.Service;
 
+import com.mensal.pizzaria.Repository.EnderecoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EnderecoService {
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
+
+
 }
