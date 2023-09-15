119ad3967873b6e8312d15959ad23d45e134e627
gustavoemf <gustavoeduardomf@gmail.com>
2023-09-15 14:41:41 -0400

    package de testes

--- src/test/java/com/mensal/pizzaria/serviceTest/clienteServiceTeste.java
+++ src/test/java/com/mensal/pizzaria/serviceTest/clienteServiceTeste.java
@@ -1,9 +1,10 @@
-package com.mensal.pizzaria.service;
+package com.mensal.pizzaria.serviceTest;
 
 
 import com.mensal.pizzaria.entity.ClienteEntity;
 import com.mensal.pizzaria.entity.EnderecoEntity;
 import com.mensal.pizzaria.repository.ClienteRepository;
+import com.mensal.pizzaria.service.ClienteService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
