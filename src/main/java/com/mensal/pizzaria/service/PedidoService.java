d8876892a7e8c4cb02ca1e6bc22d9634fd9bcf5d
Luis Rodriguez <luiserr1394@gmail.com>
2023-09-19 18:21:53 -0300

    commit regras

--- src/main/java/com/mensal/pizzaria/service/PedidoService.java
+++ src/main/java/com/mensal/pizzaria/service/PedidoService.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PedidoService {
+public class  PedidoService {
     @Autowired
     private PedidoRepository repository;
     @Autowired
